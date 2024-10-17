a47150ce851b83c9643ff7f8834f38566c557630
pillepalle127 <stefan.ruehle@gmail.com>
2024-10-17 17:59:44 +0200

    Update config.h

--- Inc/config.h
+++ Inc/config.h
@@ -74,7 +74,7 @@
 //--------------------------------------------------------------------
 //Speed settings
 #define WHEEL_CIRCUMFERENCE 2200
-#define GEAR_RATIO 11 //11 for BionX IGH3
+#define GEAR_RATIO 15 //11 for BionX IGH3
 #define SPEEDLIMIT 25
 #define PULSES_PER_REVOLUTION 1
 #define SPEEDSOURCE INTERNAL
@@ -83,8 +83,8 @@
 
 //---------------------------------------------------------------------
 //power settings
-#define PH_CURRENT_MAX 1500
-#define BATTERYCURRENT_MAX 18000
+#define PH_CURRENT_MAX 2000
+#define BATTERYCURRENT_MAX 22000
 #define REVERSE -1 //1 for normal direction, -1 for reverse
 #define PUSHASSIST_CURRENT 300
 #define VOLTAGE_MIN 1320 //33V
